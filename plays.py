3036b71936129da5c610ed957fa4f75fa8f0c7df
navneeth <navneethsridharan@gmail.com>
2019-08-10 23:32:00 -0400

    Parse play logs

--- plays.py
+++ plays.py
@@ -1,18 +1,36 @@
 import requests
+import string
 from xml.etree import ElementTree
+
+from secrets import player_alias, players
+from app import contains_word
 
 BASE_URL = 'https://boardgamegeek.com'
 
 def parse_comments(text):
-    pass
+    """
+    """
+    scores = {}
+    text = text.lower()
+    for line in text.split('\n'):
+        try:
+            player, score = tuple(word.strip() for word in line.split('-'))
+            if player in player_alias:
+                player = player_alias[player]
+            scores[player] = score
+            
+        except ValueError:
+            continue
+    return scores
 
 def fetch_plays(user="s_nav"):
     """
 
     """
+    
+
     print("Fetching plays for user {}".format(user))
-    page = 1
-    url = BASE_URL + """/xmlapi2/plays?username={}&page={}""".format(user, page)
+    url = BASE_URL + """/xmlapi2/plays?username={}&page={}""".format(user, 5)
     
     r = requests.get(url)
     root = ElementTree.fromstring(r.content)
@@ -25,6 +43,10 @@ def fetch_plays(user="s_nav"):
         except AttributeError:
             text = ""
 
-        print(date, game, game_id, text)
+        scores = parse_comments(text)
+        player_count = len(scores.keys() & players) 
+        if player_count >= 2:
+            print(date, game, game_id, scores)
+        
 
 fetch_plays()
